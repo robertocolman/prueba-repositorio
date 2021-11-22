a3e5f81c7abda8f64959f3ea49555db286e56215
robertocolman <robertocolman93@gmail.com>
2021-11-22 16:26:55 -0300

    clases

---
+++ clase_objeto.py
@@ -0,0 +1,8 @@
+#clases
+class Auto:
+	marca = ""
+	modelo = 0
+	placa = ""
+
+taxi = Auto()
+print(taxi.modelo)
---
+++ clases_objeto2.py
@@ -0,0 +1,16 @@
+#clases y objeto 2
+
+class Persona:
+	doctor = 'Roberto'
+
+print(Persona.doctor)
+
+class jugadores:
+	j1 = 'Roberto'
+	j2 = 'Lucas'
+	j3 = 'franco'
+
+print(jugadores.j3)
+
+class nombre:
+	
